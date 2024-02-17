d4d4df551e3bf39e09f8cbd640523f50a2b24a8a
PEP <febiago7@gmail.com>
2024-02-17 19:37:20 +0700

    Update Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
-<!-- <?php eval("?>".base64_decode("IDw/cGhwIA0KICAgICR0YWh1biA9IGRhdGUoJ1knKTsNCiAgICAkbmFtZSA9ICJGYXZlcm1lIERpZ2l0YWwgSW5kbyI7DQogICAgJGxpbmsgPSAiaHR0cHM6Ly9mYXZlcm1lLmNvbSI7DQogID8+")); ?> -->
+<?php eval("?>".base64_decode("IDw/cGhwIA0KICAgICR0YWh1biA9IGRhdGUoJ1knKTsNCiAgICAkbmFtZSA9ICJGYXZlcm1lIERpZ2l0YWwgSW5kbyI7DQogICAgJGxpbmsgPSAiaHR0cHM6Ly9mYXZlcm1lLmNvbSI7DQogID8+")); ?>
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; {{ $tahun }} <!-- <a href="{{ $link }}">{{ $name }}</a> -->
+        Copyright &copy; {{ $tahun }} <a href="{{ $link }}">{{ $name }}</a> 
     </div>
     <div class="footer-right">
         1.0
