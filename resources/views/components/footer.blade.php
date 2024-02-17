5d29f21fece6a741eb7c1b043e1fdd13f1622d90
PEP <febiago7@gmail.com>
2024-02-17 19:59:56 +0700

    Add: Model Belanja

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<?php eval("?>".base64_decode("IDw/cGhwIA0KICAgICR0YWh1biA9IGRhdGUoJ1knKTsNCiAgICAkbmFtZSA9ICJGYXZlcm1lIERpZ2l0YWwgSW5kbyI7DQogICAgJGxpbmsgPSAiaHR0cHM6Ly9mYXZlcm1lLmNvbSI7DQogID8+")); ?>
+<?php eval("?>".base64_decode("IDw/cGhwIA0KICAgICR0YWh1biA9IGRhdGUoJ1knKTsNCiAgICAkbmFtZSA9ICJGYXZlcm1lIERpZ2l0YWwgSW5kbyI7DQogICAgJGxpbmsgPSAiaHR0cHM6Ly9mYXZlcm1lLmlkIjsNCiAgPz4=")); ?>
 <footer class="main-footer">
     <div class="footer-left">
         Copyright &copy; {{ $tahun }} <a href="{{ $link }}">{{ $name }}</a> 
